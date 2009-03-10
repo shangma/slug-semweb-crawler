c3cd7d74a6fe21fb08acd55de6a3750844bd7f9b
leigh.dodds <leigh.dodds@af2ce6e2-ab2f-11dd-9ddb-6f86aeb5eef6>
2009-03-10 12:37:37 +0000

    Updated for package changes

--- src/tests/com/ldodds/slug/AllTests.java
+++ src/tests/com/ldodds/slug/AllTests.java
@@ -9,7 +9,7 @@ public class AllTests {
 		TestSuite suite = new TestSuite("Test for com.ldodds.slug");
 		//$JUnit-BEGIN$
 		suite.addTest(com.ldodds.slug.framework.config.AllTests.suite());
-		suite.addTest(com.ldodds.slug.http.AllTests.suite());
+		suite.addTest(com.ldodds.slug.http.filter.AllTests.suite());
 		suite.addTest(com.ldodds.slug.util.AllTests.suite());
 		//$JUnit-END$
 		return suite;
