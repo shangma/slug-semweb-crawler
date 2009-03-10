3c595a3e7759a25d8bb48b8403d5d4e6bd0ed0fa
leigh.dodds <leigh.dodds@af2ce6e2-ab2f-11dd-9ddb-6f86aeb5eef6>
2009-03-10 12:39:27 +0000

    Updated for generics, and to allow configuration of a Scanner for finding URLs

--- src/java/com/ldodds/slug/http/RDFConsumer.java
+++ src/java/com/ldodds/slug/http/RDFConsumer.java
@@ -5,31 +5,39 @@ import java.net.URL;
 import java.util.*;
 
 import com.hp.hpl.jena.rdf.model.*;
-import com.hp.hpl.jena.vocabulary.*;
 
 import com.ldodds.slug.framework.Consumer;
 import com.ldodds.slug.framework.Controller;
 import com.ldodds.slug.framework.Task;
 import com.ldodds.slug.framework.config.Memory;
 import com.ldodds.slug.framework.config.ComponentImpl;
+import com.ldodds.slug.http.scanner.Scanner;
+import com.ldodds.slug.http.scanner.SeeAlsoScanner;
+import com.ldodds.slug.vocabulary.CONFIG;
 import com.ldodds.slug.vocabulary.SCUTTERVOCAB;
 
 /**
+ * Implementation of the Consumer interface that supports parsing of RDF retrieved during the crawl.
+ * 
+ * The consumer does not store the parsed or raw data, this is the job of other consumers. This 
+ * class simply adds the number of triples to the crawlers memory, and processes the RDF to extract 
+ * <code>rdfs:seeAlso</code> links.
+ * 
  * @author ldodds
- *
  */
 public class RDFConsumer extends ComponentImpl implements Consumer
 {
-	private Memory _memory;
-	private Controller _controller;
-
+	private Memory memory;
+	private Controller controller;
+	private Scanner scanner;
+	
     public void consume(Task workItem, Object results)
     {
         URLTask urlWorkItem = (URLTask)workItem;
         Response response = (Response)results;
         
 		Resource representation = 
-			_memory.getRepresentation( urlWorkItem.getURL() );
+			memory.getRepresentation( urlWorkItem.getURL() );
         
 		if (representation == null)
 		{
@@ -41,23 +49,23 @@ public class RDFConsumer extends ComponentImpl implements Consumer
 		Model model = ModelFactory.createDefaultModel();
 		RDFReader reader = model.getReader();
 		try
-		{	
+		{
 		    reader.setErrorHandler(new LoggingErrorHandler( urlWorkItem.getURL().toString() ));
 			reader.read( model, new StringReader( response.getContent().toString() ), baseURL);
 			
-            _memory.addRawTripleCount(representation, model.size() );
+            memory.addRawTripleCount(representation, model.size() );
 			
-			Set newURLs = ModelUtils.getSeeAlsos(model, new HashSet());
-			Iterator iter = newURLs.iterator();
+			Set<URL> newURLs = findURLs(model, urlWorkItem.getURL());
+			Iterator<URL> iter = newURLs.iterator();
 			while (iter.hasNext())
 			{
-				URL next = (URL)iter.next();
+				URL next = iter.next();
                 
-				Resource rep = _memory.getOrCreateRepresentation(next, urlWorkItem.getURL());
+				Resource rep = memory.getOrCreateRepresentation(next, urlWorkItem.getURL());
 				
-                if (_memory.canBeFetched( rep, _controller.getStarted() ))
+                if (memory.canBeFetched( rep, controller.getStarted() ))
                 {
-                    _controller.addWorkItem( new URLTaskImpl(urlWorkItem, next) );
+                    controller.addWorkItem( new URLTaskImpl(urlWorkItem, next) );
                 }
                 
 			}
@@ -67,13 +75,38 @@ public class RDFConsumer extends ComponentImpl implements Consumer
 		}
     }
 
+	/**
+	 * Find URLs in the provided Model. These will be used to generate new work items.
+	 * 
+	 * @param model the model to scan for urls
+	 * @return
+	 */
+	protected Set<URL> findURLs(Model model, URL origin) {
+		return scanner.findURLs(model, origin);
+	}
+
     public void setController(Controller controller)
     {
-    	_controller = controller;
+    	this.controller = controller;
     }
     
     public void setMemory(Memory memory)
     {
-        _memory = memory;
+        this.memory = memory;
     }
+
+	@Override
+	protected boolean doConfig(Resource self) {
+		if ( self.hasProperty( CONFIG.scanner) ) {
+			scanner = (Scanner)instantiateReferenced(self, CONFIG.scanner);
+		}
+		else {
+			scanner = new SeeAlsoScanner();
+		}
+		return super.doConfig(self);
+	}
+    
+	protected Scanner getScanner() {
+		return scanner;
+	}
 }
