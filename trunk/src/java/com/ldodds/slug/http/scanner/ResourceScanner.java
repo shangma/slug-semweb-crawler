7ad3de51f890f3de4353507045d80130643c0a2f
leigh.dodds <leigh.dodds@af2ce6e2-ab2f-11dd-9ddb-6f86aeb5eef6>
2009-03-10 12:35:16 +0000

    New package for Scanners (finding links in RDF) and Storage (capturing crawled data)

---
+++ trunk/src/java/com/ldodds/slug/http/scanner/ResourceScanner.java
@@ -0,0 +1,64 @@
+package com.ldodds.slug.http.scanner;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.NodeIterator;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+
+/**
+ * Finds all HTTP Resource URIs in a Jena model
+ * 
+ * @author ldodds
+ *
+ */
+public class ResourceScanner extends ScannerImpl {
+
+	@Override
+	protected Set<URL> scan(Model toScan, URL origin) {
+		
+		Set<URL> found = new HashSet<URL>(5);
+		
+		NodeIterator objects = toScan.listObjects();
+		while ( objects.hasNext() ) {
+			//System.out.println(System.currentTimeMillis() + " node");
+			RDFNode node = objects.nextNode();
+			if ( node.isResource() && !node.isAnon() ) {
+				String uri = ((Resource)node).getURI();
+				if ( isAcceptable(uri) ) {
+					//System.out.println(System.currentTimeMillis() + " create url");					
+					URL url = createURL(uri);
+					//System.out.println(System.currentTimeMillis() + " create url finsihed");
+					if (url != null) {
+						//System.out.println(System.currentTimeMillis() + " adding");
+						found.add( url );
+						//System.out.println(System.currentTimeMillis() + " added");
+					}
+				}
+				
+			}
+			//System.out.println(System.currentTimeMillis() + " completed");
+		}
+		
+		return found;
+	}
+
+	protected URL createURL(String url) {
+		try {
+			return new URL(url);
+		} catch (MalformedURLException e) {
+			logger.log(Level.WARNING, "Unable to create url from " + url, e);
+		}
+		return null;
+	}
+
+	protected boolean isAcceptable(String uri) {
+		return uri.startsWith("http");
+	}
+
+}
