b36aa15a8925fb173748c1b04d3977b9c368da7b
leigh.dodds <leigh.dodds@af2ce6e2-ab2f-11dd-9ddb-6f86aeb5eef6>
2009-03-13 16:40:41 +0000

    Base class for components that store the Model from the Result context

---
+++ trunk/src/java/com/ldodds/slug/http/rdf/ModelStoringConsumer.java
@@ -0,0 +1,91 @@
+package com.ldodds.slug.http.rdf;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.logging.Level;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.ldodds.slug.framework.Result;
+import com.ldodds.slug.http.Response;
+import com.ldodds.slug.http.URLTask;
+import com.ldodds.slug.http.storage.DataSource;
+import com.ldodds.slug.http.storage.FileStorer;
+import com.ldodds.slug.http.storage.ResponseStorer;
+import com.ldodds.slug.vocabulary.CONFIG;
+
+public class ModelStoringConsumer extends ModelDependentConsumer {
+    private FileStorer storer;
+    
+	@Override
+	protected void processModel(URLTask task, Result result, Model model) {
+		//TODO
+		Response resp = (Response)result;
+		
+		Response response = (Response)result;
+		Resource representation =
+			getMemory().getRepresentation( response.getRequestURL() );
+		if (representation == null)
+		{
+			return;
+		}
+		
+		//TODO support other format outputs via configuration
+		DataSource source = new ModelDataSource(model, "N-TRIPLE");
+		
+		storeModel(resp, representation, source);
+		
+	}
+
+	/**
+	 * @param resp
+	 * @param representation
+	 * @param source
+	 */
+	protected void storeModel(Response resp, Resource representation,
+			DataSource source) {
+		try {
+			storer.store(getMemory(), representation, resp.getRequestURL(), source);
+		} catch (IOException e) {
+			getLogger().log(Level.SEVERE, "Unable to serialize Model", e);
+		}
+	}
+
+    protected boolean doConfig(Resource self) 
+    {
+    	//if we don't already have a cache, then look for one
+    	//in the config.
+    	File base = null;
+    	if (self.hasProperty(CONFIG.cache))
+    	{
+    		String directory = self.getProperty(CONFIG.cache).getString();
+    		base = new File(directory);
+    	}    	
+    	else
+    	{
+    		base = ResponseStorer.getDefaultCacheDir();
+    	}
+    	storer = new FileStorer(base);
+    	return true;
+    }
+	
+    public class ModelDataSource implements DataSource {
+
+    	private Model model;
+    	private String format;
+    	
+    	public ModelDataSource(Model model, String format) {
+    		this.model = model;
+    		this.format = format;
+    	}
+    	
+		public String getData() {
+
+			StringWriter writer = new StringWriter();
+			model.write(writer, format);
+			return writer.getBuffer().toString();
+		}
+    	
+    }
+}
