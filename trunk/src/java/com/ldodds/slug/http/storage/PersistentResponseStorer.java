ec97dd71cab95a58413f5d1d0050ed1693d73eb9
leigh.dodds <leigh.dodds@af2ce6e2-ab2f-11dd-9ddb-6f86aeb5eef6>
2009-03-13 16:38:43 +0000

    +comments

--- trunk/src/java/com/ldodds/slug/http/storage/PersistentResponseStorer.java
+++ trunk/src/java/com/ldodds/slug/http/storage/PersistentResponseStorer.java
@@ -7,6 +7,17 @@ import com.ldodds.slug.framework.config.Memory;
 import com.ldodds.slug.framework.config.MemoryFactory;
 import com.ldodds.slug.http.Response;
 
+/**
+ * Initial implementation of a storage component that puts data into 
+ * a persistent RDF database.
+ * 
+ * The current implementation is a bit of a hack that uses the Memory 
+ * code in order to get access to persistent Jena models. This should be 
+ * re-implemented to use the Jena Assembler API.
+ * 
+ * @author ldodds
+ * FIXME this is a hack
+ */
 public class PersistentResponseStorer extends AbstractResponseStorer
 {
   //HACK use a memory?
@@ -32,7 +43,7 @@ public class PersistentResponseStorer extends AbstractResponseStorer
       _store.load();      
     } catch (Exception e)
     {
-      _logger.log(Level.SEVERE, "Unable to load memory", e);
+      getLogger().log(Level.SEVERE, "Unable to load memory", e);
     }
     return true;
   }
