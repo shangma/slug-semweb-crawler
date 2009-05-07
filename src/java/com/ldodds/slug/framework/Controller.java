31597987b1a04248ffd5c0eb90805cb7a4734729
leigh.dodds <leigh.dodds@af2ce6e2-ab2f-11dd-9ddb-6f86aeb5eef6>
2009-05-07 10:17:34 +0000

    +new constructor Controller now manages a list of task ids and avoid re-fetches, in absence of a memory Now filters submitted tasks against known task ids Tidied up logging and reporting Shortened sleep period

--- src/java/com/ldodds/slug/framework/Controller.java
+++ src/java/com/ldodds/slug/framework/Controller.java
@@ -23,6 +23,7 @@ import java.util.logging.*;
  */
 public class Controller implements Runnable
 {
+  private Set<String> _taskIds;  
   private List<Task> _workItems;
   private WorkerFactory _factory;
   private int _numberOfWorkers;
@@ -31,62 +32,74 @@ public class Controller implements Runnable
   private Monitor _monitor;
   private ThreadGroup _workerThreadGroup;
   private Date _started;
+
+  public Controller(WorkerFactory factory, int numberOfWorkers, Monitor monitor)
+  {
+      _workItems = new ArrayList<Task>(1000);
+      _taskIds = new HashSet<String>(1000);
       
+      _factory = factory;
+      _numberOfWorkers = numberOfWorkers;
+      _workers = new ArrayList<Worker>();
+      _monitor = monitor;                
+      _logger = Logger.getLogger( getClass().getPackage().getName() );
+     
+      factory.setController(this);
+      factory.setMonitor(_monitor);
+      _monitor.setController(this);
+      
+      _workerThreadGroup = new ThreadGroup("Workers");
+  }
+  
     public Controller(List<Task> workItems, WorkerFactory factory, int numberOfWorkers, Monitor monitor)
     {
-        _workItems = new ArrayList<Task>(workItems);
-        _factory = factory;
-        _numberOfWorkers = numberOfWorkers;
-        _workers = new ArrayList<Worker>();
-        _monitor = monitor;                
-        _logger = Logger.getLogger( getClass().getPackage().getName() );
-       
-        factory.setController(this);
-        factory.setMonitor(_monitor);
-        _monitor.setController(this);
-        
-        _workerThreadGroup = new ThreadGroup("Workers");
+    	this(factory, numberOfWorkers, monitor);
+        addWorkItems(workItems, null);
     }
-    
+        
     public void run()
     {        
-      _started = Calendar.getInstance().getTime();
-        
-        _logger.log(Level.INFO, "Controller thread starting");
-        
-        _logger.fine("Starting " + _numberOfWorkers + " Workers");
-        for (int i=1; i<=_numberOfWorkers; i++)
-        {
-            Worker worker = _factory.getWorker("Worker-" + i );
-            _workers.add(worker);
-            Thread thread = new Thread(_workerThreadGroup, worker);
-            thread.start();            
-        }        
-        
-        _logger.log(Level.INFO, "Workers created");
-        
-        //now loop
-        while (true)
-        {
-            try
-            {
-                Thread.sleep(1000);
-                if (_workItems.size() == 0 && _monitor.getNumberOfActiveWorkers() == 0)
-                {
-                  break;
-                }
-            } catch (InterruptedException ie)
-            {
-                //continue loop
-            }
-        }
-        
-        _logger.log(Level.INFO, "Controller thread stopping. Task list completed");
-        stop();
+    	_started = Calendar.getInstance().getTime();
+    	_logger.log(Level.INFO, "Controller thread starting");
+    	_logger.config("Starting " + _numberOfWorkers + " Workers");
+    	
+    	for (int i=1; i<=_numberOfWorkers; i++)
+    	{
+    		Worker worker = _factory.getWorker("Worker-" + i );
+    		_workers.add(worker);
+    		Thread thread = new Thread(_workerThreadGroup, worker);
+    		thread.start();            
+    	}        
+
+    	_logger.log(Level.FINER, "Workers created");
+    	
+    	//notify?
+    	//notifyAll();
+    	
+    	//now loop
+    	while (true)
+    	{
+    		try
+    		{
+    			Thread.sleep(500);
+    			if (_workItems.size() == 0 && _monitor.getNumberOfActiveWorkers() == 0)
+    			{
+    				break;
+    			}
+    		} catch (InterruptedException ie)
+    		{
+    			//continue loop
+    		}
+    	}
+
+    	_logger.log(Level.INFO, "Controller thread stopping. Task list completed");
+    	stop();
     }
     
     public void stop()
     {
+    	//TODO dump any outstanding tasks to back-up scutterplan
+    	
         _logger.fine("Stopping " + _numberOfWorkers + " Workers");        
         for (Iterator<Worker> iter = _workers.iterator(); iter.hasNext();)
         {
@@ -102,7 +115,7 @@ public class Controller implements Runnable
     
     public void addWorkers(int increment)
     {
-        _logger.fine("Adding " + increment + " extra Workers");
+        _logger.finer("Adding " + increment + " extra Workers");
         for (int i=_numberOfWorkers; 
                i<= _numberOfWorkers + increment;
                i++)
@@ -117,7 +130,7 @@ public class Controller implements Runnable
     
     public void reduceWorkers(int decrement)
     {
-        _logger.fine("Removing " + decrement + " Workers");
+        _logger.finer("Removing " + decrement + " Workers");
         for (int i=_numberOfWorkers;
                i >= _numberOfWorkers - decrement;
                i--)
@@ -135,24 +148,39 @@ public class Controller implements Runnable
       }
       
       Object obj = _workItems.remove(0);
-      //System.out.println(obj.getClass() + ", " + obj.toString());
-        Task workItem = (Task)obj;
-        _logger.finest("Popping work item: " + workItem);
-        _monitor.startingTask(worker, workItem);
-        return workItem;
+      Task workItem = (Task)obj;
+      _logger.finest("Popping work item: " + workItem);
+      _monitor.startingTask(worker, workItem);
+      return workItem;
     }
     
-    public synchronized void completedTask(Worker worker, Task task)
+    public synchronized void completedTask(Worker worker, Task task, Result result)
     {
-      _monitor.completedTask(worker, task);
+      _monitor.completedTask(worker, task, result);
+    }
+
+    public synchronized int addWorkItems(List<Task> workItems, String source) {
+    	int added = 0;
+    	for (Task workItem : workItems) {
+    		boolean success = addWorkItem(workItem);
+    		if (success) added++;
+    	}
+    	_logger.fine("QUEUED "  + added + " tasks " + (source != null ? " from " + source : "") );
+    	return added;
     }
     
-    public synchronized void addWorkItem(Task workItem)
+    public synchronized boolean addWorkItem(Task workItem)
     {
-        _logger.fine("Adding new work item: " + workItem);        
+        _logger.finest("Adding new work item: " + workItem);
+        if ( _taskIds.contains(workItem.getId()) ) {
+        	_logger.log(Level.FINER, "REJECT: " + workItem.getId() + "(controller)");
+        	return false;
+        }
         _workItems.add(workItem);
+        _taskIds.add( workItem.getId() );
         //let the workers know
         notifyAll();
+        return true;
     }
 
   public Date getStarted()
