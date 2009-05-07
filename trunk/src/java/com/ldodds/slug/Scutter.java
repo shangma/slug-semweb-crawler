08fb5d5ec853d05aab14cfe255613e5f3543e640
leigh.dodds <leigh.dodds@af2ce6e2-ab2f-11dd-9ddb-6f86aeb5eef6>
2009-05-07 10:20:54 +0000

    Tidied up logging, reducing noise on cmd line Changed how tasks and loaded and supplied to Controller

--- trunk/src/java/com/ldodds/slug/Scutter.java
+++ trunk/src/java/com/ldodds/slug/Scutter.java
@@ -26,19 +26,19 @@ public class Scutter implements Runnable
 {
   private static final String DEFAULT_USER_AGENT = "Slug Semantic Web Crawler (http://www.ldodds.com/projects/slug)";
   
-  private List<Task> _urls;
+  private MemoryFactory memoryFactory;
   private Memory memory;
   private FilteringController controller;
   private int numberOfWorkers;
   private String configFile;
   private String scutterId;
+  private String scutterPlan;
   private boolean freshen;
   
   private static Logger logger = Logger.getLogger(Scutter.class.getPackage().getName());
   
   public Scutter() throws Exception
   {
-    _urls = new ArrayList<Task>();
     numberOfWorkers = 5;
     scutterId = "default";
     freshen = false;
@@ -77,33 +77,44 @@ public class Scutter implements Runnable
      * @throws FileNotFoundException
      * @throws MalformedURLException
      */
-  private void addScutterPlan(String scutterplan) throws FileNotFoundException, MalformedURLException {
-    Set<URLTask> urls = new HashSet<URLTask>();
-        
-    Model plan = ModelFactory.createDefaultModel();
-    //FIXME other formats
-    FileManager.get().readModel(plan, scutterplan, "TURTLE");
-    
-    //get all seeAlso links from scutter plan, and add to work plan
-    getSeeAlsoAsTasks(plan, urls);
-    
-    _urls.addAll(urls);
+  private void setScutterPlan(String scutterplan) throws FileNotFoundException, MalformedURLException {
+	logger.config("Scutter plan: " + scutterplan);
+	this.scutterPlan = scutterplan;	
   }
 
+  private List<Task> readScutterPlan() throws FileNotFoundException, MalformedURLException {
+	    Set<URLTask> tasks = new HashSet<URLTask>();
+        
+	    Model plan = ModelFactory.createDefaultModel();
+	    logger.finer("Reading scutter plan");
+	    //FIXME other formats    
+	    FileManager.get().readModel(plan, scutterPlan, "TURTLE");
+	    
+	    //get all seeAlso links from scutter plan, and add to work plan
+	    getSeeAlsoAsTasks(plan, tasks);
+	    	    
+	    List<Task> urls = new ArrayList<Task>( tasks.size() );
+	    urls.addAll(tasks);
+	    return urls;	  
+  }
+  
   public void run() {
     try {
       Model config = readConfig();
       Resource me = getSelf(config);
       
       //create and load memory of previous crawls
-      memory = new MemoryFactory().getMemoryFor(me);
+      memoryFactory = new MemoryFactory(me);
+      memory = memoryFactory.getMemory();
+      logger.finer("Crawler memory type:" + memory.getClass().getName() );
       memory.load();
+      logger.info("Crawler memory loaded");
       
-      //create the worker factory, TODO make this configurable 
+      //create the worker factory 
       URLRetrievalWorkerFactory factory = new URLRetrievalWorkerFactory();
           
       //configure the shared memory
-      factory.setMemory( memory );      
+      factory.setMemoryFactory( memoryFactory );      
       
       //what to do with fetched data      
       ComponentFactory componentFactory = new ComponentFactory();
@@ -114,22 +125,27 @@ public class Scutter implements Runnable
       consumer.setMemory(memory);      
       factory.setConsumer( consumer );
 
+      logger.finer("Crawler components configured");
+      
       //how to monitor progress, TODO make this configurable
       Monitor monitor = new MonitorImpl();
       factory.setMonitor( monitor );
                 
       //be polite and set JVM user agent
       setUserAgent(me);
-      
-      if (freshen) {
-        _urls.addAll( readURLsFromMemory() );       
-      }
-      
+            
       //how many workers?
       numberOfWorkers = me.getProperty(CONFIG.workers).getInt();
       
-      controller = new FilteringController(_urls, factory , numberOfWorkers, monitor);
+      controller = new FilteringController(factory , numberOfWorkers, monitor);
 
+      controller.addWorkItems( readScutterPlan(), "scutterplan");
+      
+      if (freshen) {
+          controller.addWorkItems( readURLsFromMemory(), "memory" );       
+      }
+
+      
       //how to filter tasks
       List<Component> filters = componentFactory.instantiateComponentsFor(me, CONFIG.filters);
       DelegatingTaskFilterImpl filter = new DelegatingTaskFilterImpl(filters);      
@@ -148,14 +164,19 @@ public class Scutter implements Runnable
 	  if ( me.hasProperty(CONFIG.userAgent) ) {
 		  userAgent = me.getProperty(CONFIG.userAgent).getObject().toString();
 	  }
+	  logger.config("User agent: " + userAgent);
       System.setProperty("http.agent", userAgent);	
   }
 
-private List<URLTask> readURLsFromMemory() throws MalformedURLException
+ private List<Task> readURLsFromMemory() throws MalformedURLException
   {
-    List<URLTask> urls = new ArrayList<URLTask>();
+	logger.finer("Reading urls from crawler memory");
+    List<Task> urls = new ArrayList<Task>();
     //loop through existing memory and add all previously found urls to work plan
     ResIterator reps = memory.getAllRepresentations();
+    if (reps == null) {
+    	return urls;
+    }
     while (reps.hasNext())
     {
       Resource representation = reps.nextResource();
@@ -195,22 +216,27 @@ private List<URLTask> readURLsFromMemory() throws MalformedURLException
   
   public void save() throws Exception
   {
-    System.out.println("Saving memory");
+    logger.finer("Saving memory");
     memory.save();
   }
   
   private void setConfig(String config) 
   {
+	logger.config("Using scutter config: " + config);
     configFile = config;
   }
   
   private void setId(String id)
   {
+	logger.config("Scutter config id: " + id);
     scutterId = id;
   }
   
   private void setFreshen(boolean freshen)
   {
+	if (freshen) {
+		logger.config("Freshening urls in memory");
+	}
     this.freshen = freshen;
   }
   
@@ -226,18 +252,15 @@ private List<URLTask> readURLsFromMemory() throws MalformedURLException
     public static void main(String[] args) throws Exception
     {      
       final Scutter scutter = new Scutter();
-      
-      System.out.println("Configuring...");     
+      logger.config("Configuring Scutter");     
       configure(scutter, args );
       
-      System.out.println("finished configuring, registering shutdown hook...");
+      logger.config("Registering shutdown hook");
     
       Runtime.getRuntime().addShutdownHook(new Thread()
             {
             public void run()
             {
-                System.out.println("HALT!");
-                Logger logger = Logger.getLogger( getClass().getPackage().getName() );
                 logger.log(Level.INFO, "Scutter manually aborted, stopping run");
                 scutter.stop();
                 try
@@ -245,18 +268,17 @@ private List<URLTask> readURLsFromMemory() throws MalformedURLException
                     scutter.save();
                 } catch (Exception e)
                 {
+                	logger.log(Level.SEVERE, "Error saving Scutter state", e);
                     e.printStackTrace();
                 }
             }
             });
     
-    System.out.println("starting scutter");
-    Thread scutterThread = new Thread(scutter);
-    scutterThread.setDaemon(false);
-    scutterThread.start();
-    System.out.println("scutter thread started");
-      //scutter.run();
-
+      logger.info("Starting Scutter");
+      Thread scutterThread = new Thread(scutter);
+      scutterThread.setDaemon(false);
+      scutterThread.start();
+      logger.info("Scutter Thread Started");
     }
     
     private static void configure(Scutter scutter, String[] args) throws Exception
@@ -272,28 +294,24 @@ private List<URLTask> readURLsFromMemory() throws MalformedURLException
 	            String arg = args[i];
 	            if (arg.equals("-config"))
 	            {
-	                System.out.println("Found Config");
 	                scutter.setConfig( args[++i] );
 	            }
 	            else if ( arg.equals("-id") )
 	            {
-	                System.out.println("Found Id");
 	                scutter.setId( args[++i] );             
 	            }
 	            else if (arg.equals("-plan"))
 	            {
-	                System.out.println("Found plan");
-	                scutter.addScutterPlan(args[++i]);
+	                scutter.setScutterPlan(args[++i]);
 	            }
 	            else if (arg.equals("-freshen"))
 	            {
-	                System.out.println("Found freshen");
 	              scutter.setFreshen(true);
 	              ++i;
 	            }
 	            else
 	            {
-	                System.out.println("Unknown argument '" + arg + "' ignored");
+	                System.err.println("Unknown argument '" + arg + "' ignored");
 	            }
 	        }
         } catch (Exception e) {
