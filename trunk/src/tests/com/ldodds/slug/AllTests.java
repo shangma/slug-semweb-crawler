b2eea7be1e59a0b587585c616f72763b964f366d
leigh.dodds <leigh.dodds@af2ce6e2-ab2f-11dd-9ddb-6f86aeb5eef6>
2009-05-08 11:03:36 +0000

    Updated for latest tests

--- trunk/src/tests/com/ldodds/slug/AllTests.java
+++ trunk/src/tests/com/ldodds/slug/AllTests.java
@@ -7,10 +7,11 @@ public class AllTests {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Test for com.ldodds.slug");
-		//$JUnit-BEGIN$
-		suite.addTest(com.ldodds.slug.framework.config.AllTests.suite());
-		suite.addTest(com.ldodds.slug.http.filter.AllTests.suite());
+		suite.addTest(com.ldodds.slug.framework.AllTests.suite());
+		suite.addTest(com.ldodds.slug.http.AllTests.suite());
 		suite.addTest(com.ldodds.slug.util.AllTests.suite());
+		
+		//$JUnit-BEGIN$
 		//$JUnit-END$
 		return suite;
 	}
