55106233c1e202312dbb71a1d8028f6466f340e9
leigh.dodds <leigh.dodds@af2ce6e2-ab2f-11dd-9ddb-6f86aeb5eef6>
2009-07-24 07:46:42 +0000

    Remove canBeFetched

--- trunk/src/java/com/ldodds/slug/http/rdf/TaskGeneratingConsumer.java
+++ trunk/src/java/com/ldodds/slug/http/rdf/TaskGeneratingConsumer.java
@@ -1,12 +1,15 @@
 package com.ldodds.slug.http.rdf;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.ldodds.slug.framework.Result;
+import com.ldodds.slug.framework.Task;
 import com.ldodds.slug.http.URLTask;
 import com.ldodds.slug.http.URLTaskImpl;
 import com.ldodds.slug.http.scanner.Scanner;
@@ -47,21 +50,12 @@ public class TaskGeneratingConsumer extends ModelDependentConsumer {
 	protected void findWorkItems(URLTask origin, Model model) {
 		Set<URL> newURLs = findURLs(model, origin.getURL());
 		Iterator<URL> iter = newURLs.iterator();
-		int addedTasks = 0;
+		List<Task> newTasks = new ArrayList<Task>(newURLs.size());
 		while (iter.hasNext()) {
 			URL next = iter.next();
-
-			Resource rep = memory.getOrCreateRepresentation(next,
-					origin.getURL());
-		
-			//TODO: this is now redundant as the Controller will never redo any task in a single run
-			//leaving for now, to allow for additional behaviour to be hooked in
-			if ( memory.canBeFetched(rep, controller.getStarted()) ) {
-				boolean added = controller.addWorkItem(new URLTaskImpl(origin, next));
-				if (added) addedTasks++;
-			}
-
+			newTasks.add( new URLTaskImpl(origin, next) );
 		}
+		int addedTasks = controller.addWorkItems(newTasks, origin.getURL().toString());		
 		getLogger().fine("QUEUED " + addedTasks + " tasks from " + origin.getURL() );
 	}
 
